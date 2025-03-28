32acff1ccbdb2fa9e1a1c7cabb7cce8f9b655ac9
Xdoxgg <145697777+Xdoxgg@users.noreply.github.com>
2025-03-28 14:39:32 +0300

    try to fix

--- Program.cs
+++ Program.cs
@@ -10,9 +10,15 @@ static class Program
     {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Form1 l= new Form1();
-        Application.Run(new Form1());
 
+
+        List<Ordere> list = new List<Ordere>()
+        {
+            new Ordere(2, new DateTime(2025, 12, 12), "adress", true, new Customer("11", "phone", "name", true),
+                new List<OrderLine>() { new OrderLine(2, new Item("123456", "item", 12.22)) })
+        };
+        DataProcessing.WriteToFile<Ordere>(list);
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
     }
 }
