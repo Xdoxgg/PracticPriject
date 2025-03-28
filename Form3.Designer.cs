55dee9e229aa8eda97b533c7fdad39e3f5a202ea
De_Lucio <kvartyra125@gmail.com>
2025-03-28 14:39:55 +0300

    added Form3 to add customers

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -30,6 +30,17 @@
         {
             button1 = new Button();
             label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            button2 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -51,16 +62,115 @@
             label1.TabIndex = 2;
             label1.Text = "Добавить покупателя";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(33, 82);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(184, 82);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(143, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(333, 82);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(169, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(508, 57);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(173, 71);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Привелегия";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(8, 43);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(142, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "Нет привелегии";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(8, 18);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(145, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.Text = "Есть привелегия";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(294, 161);
+            button2.Name = "button2";
+            button2.Size = new Size(126, 43);
+            button2.TabIndex = 7;
+            button2.Text = "Добавить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 20);
+            label2.TabIndex = 8;
+            label2.Text = "ID покупателя";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(184, 59);
+            label3.Name = "label3";
+            label3.Size = new Size(127, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Номер телефона";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(332, 59);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 20);
+            label4.TabIndex = 10;
+            label4.Text = "ФИО";
+            // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(704, 325);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form3";
             Text = "Form3";
             Load += Form3_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -69,5 +179,15 @@
 
         private Button button1;
         private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
