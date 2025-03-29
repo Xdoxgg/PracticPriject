3a108cd45d1703fb31d2ea42d37f61b5353a6ac1
De_Lucio <kvartyra125@gmail.com>
2025-03-29 09:00:56 +0300

    development Form4

--- Form4.cs
+++ Form4.cs
@@ -16,5 +16,37 @@ namespace lab2
         {
             InitializeComponent();
         }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var ord = DataProcessing.ReadJsonFromFile<Customer>();
+                var cust = ord.Select(ord => ord.FullName).ToList();
+                comboBox1.DataSource = cust;
+                var orders = DataProcessing.ReadJsonFromFile<Item>();
+                var names = orders.Select(order => order.Name).ToList();
+                comboBox2.DataSource = names;
+            } catch(Exception exc)
+            { 
+                MessageBox.Show(exc.Message); 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ///TODO: доделать переход на Form1
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
     }
 }
